8ed37f7b59d953af7ac80eb1b1cb234b71e2b69d
cmlflynn <cmlflynn@gmail.com>
2022-04-11 13:40:23 +1000

    Add files via upload
    renamed some switches for clarity, added description of program for the argsparser

--- radiometer_eqn.py
+++ radiometer_eqn.py
@@ -2,12 +2,12 @@ import numpy as np
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Short sample app')
+parser = argparse.ArgumentParser(description='Compute signal to noise of a single pulse of peak flux density S')
 
-parser.add_argument('-t', action="store", dest="Tsys", type=float)
+parser.add_argument('-tsys', action="store", dest="Tsys", type=float)
 parser.add_argument('-dt', action="store", dest="dt", type=float)
-parser.add_argument('-g', action="store", dest="gain", type=float)
-parser.add_argument('-s', action="store", dest="S", type=float)
+parser.add_argument('-G', action="store", dest="gain", type=float)
+parser.add_argument('-S', action="store", dest="S", type=float)
 parser.add_argument('-bw', action="store", dest="bw", type=float)
 parser.add_argument('-np', action="store", dest="np", type=int)
 
